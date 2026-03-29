fe135f5aaab9570a6e606b471321ac2d86d5c2b1
lijiaxiang <jiaxiang@itljx.cn>
2026-03-30 00:17:42 +0800

    增加，删除，修改（禁用启用，修改内容），根据类型查询的‘分类’业务

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -1,16 +1,18 @@
 package com.sky.controller.admin;
 
 
+import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/admin/category")
@@ -19,11 +21,67 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    /**
+     * 分类分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
     @GetMapping("/page")
     public Result<PageResult> catPage( CategoryPageQueryDTO categoryPageQueryDTO){
         log.info("分页查询分类信息:{}", categoryPageQueryDTO);
         PageResult pageResult= categoryService.catPage(categoryPageQueryDTO);
         return Result.success(pageResult);
 
+    }
+
+    /**
+     * 删除分类
+     * @param id
+     * @return
+     */
+    @DeleteMapping
+    public Result delete(Long id){
+        log.info("删除分类：{}", id);
+        categoryService.delete(id);
+        return Result.success();
+
+    }
+
+    /**
+     * 新增分类
+     * @param categoryDTO
+     * @return
+     */
+    @PostMapping
+    public Result save(@RequestBody CategoryDTO categoryDTO){
+        log.info("新增分类：{}", categoryDTO);
+        categoryService.save(categoryDTO);
+        return Result.success();
+
+    }
+
+    @PostMapping("status/{status}")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("启用或禁用分类：{}", id);
+        categoryService.startOrStop(id, status);
+        return Result.success();
+    }
+
+    @PutMapping
+    public Result update(@RequestBody CategoryDTO categoryDTO){
+        log.info("修改分类：{}", categoryDTO);
+        categoryService.update(categoryDTO);
+        return Result.success();
+    }
+    /**
+     * 根据类型查询分类
+     * @param type
+     * @return
+     */
+    @GetMapping("/list")
+    public Result<List> typeList(Integer  type){
+        log.info("根据类型查询分类：{}", type);
+        List<Category> list=categoryService.typeList(type);
+        return Result.success(list);
     }
 }
--- sky-server/src/main/java/com/sky/service/CategoryService.java
+++ sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -1,7 +1,11 @@
 package com.sky.service;
 
+import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
 import com.sky.result.PageResult;
+
+import java.util.List;
 
 /**
  * 分类业务接口
@@ -13,4 +17,35 @@ public interface CategoryService {
      * @return
      */
     PageResult catPage(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    /**
+     * 删除分类
+     */
+    void delete(Long id);
+
+    /**
+     * 新增分类
+     * @param categoryDTO
+     */
+    void save(CategoryDTO categoryDTO);
+
+    /**
+     * 启用或禁用分类
+     * @param id
+     * @param status
+     */
+    void startOrStop(Long id, Integer status);
+
+    /**
+     * 修改分类
+     * @param categoryDTO
+     */
+    void update(CategoryDTO categoryDTO);
+
+    /**
+     * 根据类型查询分类列表
+     * @param type
+     * @return
+     */
+    List<Category> typeList(Integer type);
 }
